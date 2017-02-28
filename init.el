47b92aea15e1d4d46ba83e54680691fdf86c8d10
Thomas Marlow <tom@Thomass-MacBook-Pro.local>
2017-02-28 13:52:31 -0500

    added pandoc mode and autocompletion

--- init.el
+++ init.el
@@ -64,6 +64,7 @@
                         company
                         ess
                         markdown-mode
+                        pandoc-mode
                         polymode
                         eval-in-repl
                         haskell-mode
@@ -96,6 +97,9 @@
     (when (not (package-installed-p package))
       (package-install package))))
 
+;; set PATH to shell path so that things like pandoc work correctly
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -108,7 +112,7 @@
  '(custom-safe-themes
    (quote
     ("ace9f12e0c00f983068910d9025eefeb5ea7a711e774ee8bb2af5f7376018ad2" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
- '(package-selected-packages (quote (magit)))
+ '(package-selected-packages (quote (pandoc-mode pandoc writeroom-mode magit)))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -192,13 +196,206 @@
 
 
 ;; sublimity mode - sublime text minimap and smooth scroll
-(require 'sublimity)
+;;(require 'sublimity)
 ;;(require 'sublimity-scroll)
-(require 'sublimity-map)
+;;(require 'sublimity-map)
 ;;(require 'sublimity-attractive)
 
-(sublimity-mode 1)
+;;(sublimity-mode 1)
 
 ;; pairs parathenses and quotations
 (require 'autopair)
   (autopair-global-mode) ;; to enable in all buffers
+
+;; ;; Make control-z undo
+(global-undo-tree-mode t)
+(global-set-key (kbd "C-z") 'undo)
+(define-key undo-tree-map (kbd "C-S-z") 'undo-tree-redo)
+(define-key undo-tree-map (kbd "C-x u") 'undo)
+(define-key undo-tree-map (kbd "C-x U") 'undo-tree-visualize)
+(define-key undo-tree-map (kbd "M-z") 'undo-tree-visualize)
+;; Make C-g quit undo tree
+(define-key undo-tree-visualizer-mode-map (kbd "C-g") 'undo-tree-visualizer-quit)
+(define-key undo-tree-visualizer-mode-map (kbd "<escape> <escape> <escape>") 'undo-tree-visualizer-quit)
+
+
+
+;; ;; 
+;; Make right-click do something close to what people expect
+(global-set-key (kbd "<mouse-3>") 'mouse3-popup-menu)
+;; (global-set-key (kbd "C-f") 'isearch-forward)
+;; (global-set-key (kbd "C-s") 'save-buffer)
+;; (global-set-key (kbd "C-o") 'counsel-find-file)
+(define-key cua-global-keymap (kbd "<C-S-SPC>") nil)
+(define-key cua-global-keymap (kbd "<C-return>") nil)
+(setq cua-rectangle-mark-key (kbd "<C-S-SPC>"))
+(define-key cua-global-keymap (kbd "<C-S-SPC>") 'cua-rectangle-mark-mode)
+
+;; Undo/redo window changes
+(winner-mode 1)
+
+;; use ace-window for navigating windows
+(global-set-key (kbd "C-x O") 'ace-window)
+(with-eval-after-load "ace-window"
+  (set-face-attribute 'aw-leading-char-face nil :height 2.5))
+
+;; enable on-the-fly spell checking
+(add-hook 'text-mode-hook
+          (lambda ()
+            (flyspell-mode 1)))
+;; prevent flyspell from finding mistakes in the code
+(add-hook 'prog-mode-hook
+          (lambda ()
+            ;; `ispell-comments-and-strings'
+            (flyspell-prog-mode)))
+
+;; ispell should not check code blocks in org mode
+(add-to-list 'ispell-skip-region-alist '(":\\(PROPERTIES\\|LOGBOOK\\):" . ":END:"))
+(add-to-list 'ispell-skip-region-alist '("#\\+BEGIN_SRC" . "#\\+END_SRC"))
+(add-to-list 'ispell-skip-region-alist '("#\\+begin_src" . "#\\+end_src"))
+(add-to-list 'ispell-skip-region-alist '("^#\\+begin_example " . "#\\+end_example$"))
+(add-to-list 'ispell-skip-region-alist '("^#\\+BEGIN_EXAMPLE " . "#\\+END_EXAMPLE$"))
+
+;; Dictionaries
+
+;; default in case we don't find something local
+
+(global-set-key (kbd "C-c d") 'define-word-at-point)
+(global-set-key (kbd "C-c S-D") 'define-word)
+
+;; use dictionary app on os x
+(when (memq window-system '(mac ns))
+  (global-set-key (kbd "C-c d") 'osx-dictionary-search-word-at-point)
+  (global-set-key (kbd "C-c S-D") 'osx-dictionary-search-input))
+
+
+;; Ivy-based interface to standard commands
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "C-r") 'swiper)
+;; Search files in directory with C-S
+(global-set-key (kbd "C-S-s") 'find-grep-dired); default if we don't find something better
+(cond
+ ((executable-find "rg") ; search with ripgrep if we have it
+  (global-set-key (kbd "C-S-s") 'counsel-rg))
+ ((executable-find "ag") ; otherwise search with ag if we have it
+  (global-set-key (kbd "C-S-s") 'counsel-ag))
+ ((executable-find "pt") ; otherwise search with pt if we have it
+  (global-set-key (kbd "C-S-s") 'counsel-pt)))
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "M-y") 'counsel-yank-pop)
+(global-set-key (kbd "C-S-v") 'counsel-yank-pop)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-o") 'counsel-find-file)
+;; search for files to open with "C-O=
+(when (memq window-system '(mac ns)) ; use mdfind on Mac. TODO: what about windows?
+  (setq locate-command "mdfind")
+  (setq counsel-locate-cmd 'counsel-locate-cmd-mdfind))
+(global-set-key (kbd "C-x C-S-F") 'find-name-dired) ; default in case we don't have something better
+(global-set-key (kbd "C-x C-S-F") 'counsel-locate)
+(global-set-key (kbd "C-S-O") 'counsel-locate)
+(global-set-key (kbd "C-x C-r") 'counsel-recentf)
+(global-set-key (kbd "<C-tab>") 'counsel-company)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-load-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+;; Ivy-based interface to shell and system tools
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+;; Ivy-resume and other commands
+
+(global-set-key (kbd "C-c i") 'ivy-resume)
+
+;; Make Ivy more like ido
+;;(define-key ivy-minibuffer-map (kbd "<return>") 'ivy-alt-done)
+;;(define-key ivy-minibuffer-map (kbd "C-d") 'ivy-done)
+;;(define-key ivy-minibuffer-map (kbd "C-b") 'ivy-immediate-done)
+;;(define-key ivy-minibuffer-map (kbd "C-f") 'ivy-immediate-done)
+
+;; show recently opened files
+(setq recentf-max-menu-items 50)
+(recentf-mode 1)
+
+(require 'company)
+;; cancel if input doesn't match, be patient, and don't complete automatically.
+(setq company-require-match nil
+      company-async-timeout 6
+      company-idle-delay nil)
+;; complete using C-tab
+(global-set-key (kbd "<C-tab>") 'counsel-company)
+;; use C-n and C-p to cycle through completions
+;; (define-key company-mode-map (kbd "<tab>") 'company-complete)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "<tab>") 'company-complete-common)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-active-map (kbd "<backtab>") 'company-select-previous)
+
+(require 'company-capf)
+;; put company-capf and company-files at the beginning of the list
+(setq company-backends
+      '(company-files company-capf company-nxml company-css company-cmake company-semantic company-clang company-xcode company-eclim))
+(setq-default company-backends
+              '(company-files company-capf company-nxml company-css company-cmake company-semantic company-clang company-xcode company-eclim))
+
+;;Use tab to complete.
+;; See https://github.com/company-mode/company-mode/issues/94 for another approach.
+
+;; this is a copy-paste from the company-package with extra conditions to make
+;; sure we don't offer completions in the middle of a word.
+
+(defun my-company-indent-or-complete-common ()
+  "Indent the current line or region, or complete the common part."
+  (interactive)
+  (cond
+   ((use-region-p)
+    (indent-region (region-beginning) (region-end)))
+   ((and (not (looking-at "\\w\\|\\s_"))
+         (memq indent-line-function
+               '(indent-relative indent-relative-maybe)))
+    (company-complete-common))
+   ((let ((old-point (point))
+          (old-tick (buffer-chars-modified-tick))
+          (tab-always-indent t))
+      (call-interactively #'indent-for-tab-command)
+      (when (and (eq old-point (point))
+                 (eq old-tick (buffer-chars-modified-tick))
+                 (not (looking-at "\\w\\|\\s_")))
+        (company-complete-common))))))
+
+(define-key company-mode-map (kbd "<tab>") 'my-company-indent-or-complete-common)
+
+;; not sure why this should be set in a hook, but that is how the manual says to do it.
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; (require 'which-key)
+(which-key-mode)
+
+;; (require 'flycheck)
+(global-flycheck-mode)
+
+;;; Configure outline minor modes
+;; Less crazy key bindings for outline-minor-mode
+(setq outline-minor-mode-prefix "\C-c\C-o")
+;; load outline-magic along with outline-minor-mode
+(add-hook 'outline-minor-mode-hook 
+          (lambda () 
+            (require 'outline-magic)
+            (define-key outline-minor-mode-map "\C-c\C-o\t" 'outline-cycle)))
+
+;; require the main file containing common functions
+(require 'eval-in-repl)
+(setq comint-process-echoes t)
+
+;; truncate lines in comint buffers
+(add-hook 'comint-mode-hook
+          (lambda()
+            (setq truncate-lines 1)))
+
+;; Scroll down for input and output
+(setq comint-scroll-to-bottom-on-input t)
+(setq comint-scroll-to-bottom-on-output t)
+(setq comint-move-point-for-output t)
